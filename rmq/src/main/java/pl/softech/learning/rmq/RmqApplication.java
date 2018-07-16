698db78a3f1961dd30b74aa8c0a73f0a84964d12
Sławomir Śledź <slawomir.sledz@gmail.com>
2018-07-16 13:51:26 +0200

    Initial version

---
+++ rmq/src/main/java/pl/softech/learning/rmq/RmqApplication.java
@@ -0,0 +1,12 @@
+package pl.softech.learning.rmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmqApplication.class, args);
+	}
+}
