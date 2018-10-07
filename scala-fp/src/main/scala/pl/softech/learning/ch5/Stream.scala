757ddf3994373a0447281ed6648286b097a4e459
Sławomir Śledź <slawomir.sledz@gmail.com>
2018-10-07 14:53:02 +0200

    playing with Stream & Trees

---
+++ scala-fp/src/main/scala/pl/softech/learning/ch5/Stream.scala
@@ -0,0 +1,28 @@
+package pl.softech.learning.ch5
+
+sealed trait Stream[+A] {
+  def headOption: Option[A] = this match {
+    case Empty => None
+    case Cons(h, t) => Some(h())
+  }
+}
+
+case object Empty extends Stream[Nothing]
+
+case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+
+object Stream {
+  def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+    lazy val head = hd
+    lazy val tail = tl
+    Cons(() => head, () => tail)
+  }
+
+  def empty[A]: Stream[A] = Empty
+
+  def apply[A](as: A*): Stream[A] =
+    if (as.isEmpty) empty
+    else cons(as.head, apply(as.tail: _*))
+
+  object Implicits extends Ex1.Implicits
+}
