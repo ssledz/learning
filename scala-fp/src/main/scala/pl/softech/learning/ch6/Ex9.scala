42e27a27669bcf9c0456a63d34b2cdd3b7f2262e
Sławomir Śledź <slawomir.sledz@gmail.com>
2018-10-27 22:58:14 +0200

    playing with state

--- scala-fp/src/main/scala/pl/softech/learning/ch6/Ex9.scala
+++ scala-fp/src/main/scala/pl/softech/learning/ch6/Ex9.scala
@@ -1,15 +1,24 @@
 package pl.softech.learning.ch6
 
+import pl.softech.learning.ch6.RNG.Implicits._
+import pl.softech.learning.ch6.RNG.Rand
+import pl.softech.learning.ch6.RNG._
+
 object Ex9 {
 
-  trait Implicits {
+  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = for {
+    x <- ra
+    y <- rb
+  } yield f(x, y)
 
-    implicit class Ex9Opts(r: RNG) {
-    }
-
-  }
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] = s.flatMap(x => rng => (f(x), rng))
 
   def main(args: Array[String]): Unit = {
+    val r = new SimpleRNG(1)
+    println(map2(int, int)(_ + _)(r))
+    println(int.map2(int)(_ + _)(r))
+    println(int.map(_ + 1)(r))
+    println(map(int)(_ + 1)(r))
 
   }
 
