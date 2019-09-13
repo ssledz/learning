dfbafbdf1eebedfd531d9a283ee9463727afc285
Sławomir Śledź <slawomir.sledz@gmail.com>
2019-09-13 18:57:00 +0200

    bzz

--- android/an-kotlin-playground/app/src/main/java/io/github/ssledz/app/ui/activities/MainActivity.kt
+++ android/an-kotlin-playground/app/src/main/java/io/github/ssledz/app/ui/activities/MainActivity.kt
@@ -1,6 +1,9 @@
 package io.github.ssledz.app.ui.activities
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import io.github.ssledz.app.R
 import kotlinx.android.synthetic.main.activity_main.*
@@ -10,6 +13,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        message.text = "Hello Kotlin!"
+        message.text = "Hello Kotlin!!!"
+
+        Log.i("info", "Done creating the app")
+
+    }
+
+    fun onClick(v : View?) {
+
+        Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show()
+        Log.i("info", "User clicked")
+
     }
 }
