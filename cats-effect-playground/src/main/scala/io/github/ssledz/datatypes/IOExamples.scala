cae95ee02bb9512d23cf12677a1aa19c79656d86
Sławomir Śledź <slawomir.sledz@gmail.com>
2020-02-25 10:53:36 +0100

    more exemaples for IO data type

--- cats-effect-playground/src/main/scala/io/github/ssledz/datatypes/IOExamples.scala
+++ cats-effect-playground/src/main/scala/io/github/ssledz/datatypes/IOExamples.scala
@@ -1,19 +1,71 @@
 package io.github.ssledz.datatypes
 
+import java.util.concurrent.atomic.AtomicBoolean
 import java.util.concurrent.{Executors, ScheduledExecutorService}
 
-import cats.effect.{ContextShift, ExitCode, IO, IOApp}
+import cats.effect.{ContextShift, ExitCode, IO, IOApp, Timer}
 import cats.implicits._
 
-import scala.concurrent.Future
 import scala.concurrent.duration._
+import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success}
 object IOExamples extends IOApp {
 
   def run(args: List[String]): IO[ExitCode] =
 //    StackSafety.app.as(ExitCode.Success)
 //    SyncEffects.app.as(ExitCode.Success)
-    AsyncEffects.app.as(ExitCode.Success)
+//    AsyncEffects.app.as(ExitCode.Success)
+    CancelableProcesses.app.as(ExitCode.Success)
+
+}
+
+object CancelableProcesses {
+
+  def slowIteration(n: Int, label: String, duration: FiniteDuration = 500.millis)(implicit cs: ContextShift[IO], timer: Timer[IO]): IO[Unit] = IO.suspend {
+    if (n <= 0) {
+      putStrLn(s"$label: $n and finished")
+    } else putStrLn(s"$label: $n") >> timer.sleep(duration) >> slowIteration(n - 1, label)
+  }
+
+  // Cancellation status is only checked after asynchronous boundaries (in the previous example timer.sleep played the role of async boundary)
+  def notCancelableSlowIteration(n: Int, label: String)(implicit cs: ContextShift[IO]): IO[Unit] = IO.suspend {
+    if (n <= 0) {
+      putStrLn(s"$label: $n and finished")
+    } else putStrLn(s"$label: $n") >> notCancelableSlowIteration(n - 1, label)
+  }
+
+  def slowIteration2(n: Int, label: String, duration: FiniteDuration = 500.millis)(implicit ec: ExecutionContext): IO[Unit] = {
+    val go = new AtomicBoolean(true)
+    def inpureIter(n: Int): Unit = {
+      def print(m: String) = println(s"[$currentThread] $m")
+      var iter = n
+      while (iter > 0 && go.get()) {
+        print(s"$label: $iter")
+        iter = iter - 1
+        Thread.sleep(duration.toMillis)
+      }
+      print(s"$label: $iter and finished")
+    }
+    IO.cancelable { cb =>
+      ec.execute(() => cb(Right(inpureIter(n))))
+      IO(go.set(false)).void
+    }
+
+  }
+
+  def currentThread: String = Thread.currentThread().getName
+
+  def putStrLn(m: String): IO[Unit] = IO(println(s"[$currentThread] $m"))
+
+  def app(implicit cs: ContextShift[IO], timer: Timer[IO]): IO[Unit] = {
+    import concurrent.ExecutionContext.Implicits.global
+    for {
+      _ <- slowIteration(3, "it from 3")
+      _ <- IO.race(slowIteration(3, "it from 3"), slowIteration(5, "it from 5"))
+      _ <- IO.race(notCancelableSlowIteration(3, "not cancelable it from 3"), notCancelableSlowIteration(5, "not cancelable it from 5"))
+      _ <- IO.race(slowIteration2(3, "it2 from 3"), slowIteration2(5, "it2 from 5"))
+    } yield ()
+  }
 
 }
 
@@ -21,12 +73,11 @@ object AsyncEffects {
 
   import concurrent.ExecutionContext.Implicits.global
 
-  implicit val se = Executors.newScheduledThreadPool(2)
-
   val future1 = convert(Future(println("future 1")))
   val future2 = convert(Future(println("future 2")))
 
   def app(implicit cs: ContextShift[IO]): IO[Unit] = {
+    implicit val se = Executors.newScheduledThreadPool(2)
     val future3 = IO.fromFuture(IO(Future(println("future 3"))))
     val future4 = IO.fromFuture(IO.pure(Future(println("future 4")))) // eager evaluation
     for {
