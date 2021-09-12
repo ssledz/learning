f5ffc94fc1aafe2099d52970d84b42feb5b3be6d
Sławomir Śledź <slawomir.sledz@gmail.com>
2021-09-12 13:45:38 +0200

    servant playground

---
+++ servant-playground/README.md
@@ -0,0 +1,125 @@
+## having sources
+
+```
+hasktags . ~/git/ghc/libraries ~/git/servant
+```
+
+## generating hie.yaml
+
+```
+cabal install implicit-hie
+gen-hie > hie.yaml
+```
+
+## other
+
+```
+cabal list --installed
+```
+
+# Examples
+
+## app1
+
+```
+curl http://localhost:8081/users | jq
+```
+
+```
+[
+ {
+  "name": "Isaac Newton",
+  "email": "isaac@newton.co.uk",
+  "age": 372,
+  "registration_date": "1683-03-01"
+ },
+ {
+  "name": "Albert Einstein",
+  "email": "ae@mc2.org",
+  "age": 136,
+  "registration_date": "1905-12-01"
+ }
+]
+```
+
+## app2
+
+```
+curl http://localhost:8081/albert | jq
+```
+
+```
+{
+  "name": "Albert Einstein",
+  "email": "ae@mc2.org",
+  "age": 136,
+  "registration_date": "1905-12-01"
+}
+```
+
+## app3
+
+```
+curl http://localhost:8081/position/1/2 | jq
+```
+
+```
+{
+  "yCoord": 2,
+  "xCoord": 1
+}
+```
+
+```
+curl -X POST -d '{"clientName":"Alp Mestanogullari", "clientEmail" : "alp@foo.com", "clientAge": 25, "clientInterestedIn": ["haskell", "mathematics"]}' -H 'Accept: application/json' -H 'Content-type: application/json' http://localhost:8081/marketing | jq
+```
+
+```
+{
+  "to": "alp@foo.com",
+  "body": "Hi Alp Mestanogullari,\n\nSince you've recently turned 25, have you checked out our latest haskell, mathematics products? Give us a visit!",
+  "subject": "Hey Alp Mestanogullari, we miss you!",
+  "from": "great@company.com"
+}
+```
+
+## app4
+
+```
+curl http://localhost:8081/persons
+```
+
+```
+[
+  {
+    "firstName": "Isaac",
+    "lastName": "Newton"
+  },
+  {
+    "firstName": "Albert",
+    "lastName": "Einstein"
+  }
+]
+```
+
+```
+curl -H 'Accept: text/html' http://localhost:8081/persons | tidy -i
+```
+
+```
+  <table>
+    <tr>
+      <th>first name</th>
+      <th>last name</th>
+    </tr>
+    <tr>
+      <td>Isaac</td>
+      <td>Newton</td>
+    </tr>
+    <tr>
+      <td>Albert</td>
+      <td>Einstein</td>
+    </tr>
+  </table>
+
+```
---
+++ servant-playground/src/Server.hs
@@ -0,0 +1,234 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
+
+module Server (runApp, app1, app2, app3, app4) where
+
+import Prelude ()
+import Prelude.Compat
+
+import Control.Monad.Except
+import Control.Monad.Reader
+import Data.Aeson
+import Data.Aeson.Types
+import Data.Attoparsec.ByteString
+import Data.ByteString (ByteString)
+import Data.List
+import Data.Maybe
+import Data.String.Conversions
+import Data.Time.Calendar
+import GHC.Generics
+import Lucid
+import Network.HTTP.Media ((//), (/:))
+import Network.Wai
+import Network.Wai.Handler.Warp
+import Servant
+import System.Directory
+import Text.Blaze
+import Text.Blaze.Html.Renderer.Utf8
+import Servant.Types.SourceT (source)
+import qualified Data.Aeson.Parser
+import qualified Text.Blaze.Html
+
+type UserAPI1 = "users" :> Get '[JSON] [User]
+
+data User = User
+  { name :: String
+  , age :: Int
+  , email :: String
+  , registration_date :: Day
+  } deriving (Eq, Show, Generic)
+
+instance ToJSON User
+
+users1 :: [User]
+users1 =
+  [ User "Isaac Newton"    372 "isaac@newton.co.uk" (fromGregorian 1683  3 1)
+  , User "Albert Einstein" 136 "ae@mc2.org"         (fromGregorian 1905 12 1)
+  ]
+
+server1 :: Server UserAPI1
+server1 = return users1  
+
+userAPI :: Proxy UserAPI1
+userAPI = Proxy
+
+-- 'serve' comes from servant and hands you a WAI Application,
+-- which you can think of as an "abstract" web application,
+-- not yet a webserver.
+app1 :: Application
+app1 = serve userAPI server1
+
+runApp :: Application -> Int -> IO ()
+runApp app port = run port app 
+
+-------------------
+-- App2
+--------------------
+
+type UserAPI2 = "users" :> Get '[JSON] [User]
+           :<|> "albert" :> Get '[JSON] User
+           :<|> "isaac" :> Get '[JSON] User
+
+isaac :: User
+isaac = User "Isaac Newton" 372 "isaac@newton.co.uk" (fromGregorian 1683 3 1)
+
+albert :: User
+albert = User "Albert Einstein" 136 "ae@mc2.org" (fromGregorian 1905 12 1)
+
+users2 :: [User]
+users2 = [isaac, albert]
+
+server2 :: Server UserAPI2
+server2 = return users2
+     :<|> return albert
+     :<|> return isaac
+
+userAPI2 :: Proxy UserAPI2
+userAPI2 = Proxy
+
+app2 :: Application
+app2 = serve userAPI2 server2
+
+---------------------
+-- App3
+---------------------
+
+type API = "position" :> Capture "x" Int :> Capture "y" Int :> Get '[JSON] Position
+      :<|> "hello" :> QueryParam "name" String :> Get '[JSON] HelloMessage
+      :<|> "marketing" :> ReqBody '[JSON] ClientInfo :> Post '[JSON] Email
+
+data Position = Position
+  { xCoord :: Int
+  , yCoord :: Int
+  } deriving Generic
+
+instance ToJSON Position
+
+newtype HelloMessage = HelloMessage { msg :: String }
+  deriving Generic
+
+instance ToJSON HelloMessage
+
+data ClientInfo = ClientInfo
+  { clientName :: String
+  , clientEmail :: String
+  , clientAge :: Int
+  , clientInterestedIn :: [String]
+  } deriving Generic
+
+instance FromJSON ClientInfo
+instance ToJSON ClientInfo
+
+data Email = Email
+  { from :: String
+  , to :: String
+  , subject :: String
+  , body :: String
+  } deriving Generic
+
+instance ToJSON Email
+
+emailForClient :: ClientInfo -> Email
+emailForClient c = Email from' to' subject' body'
+
+  where from'    = "great@company.com"
+        to'      = clientEmail c
+        subject' = "Hey " ++ clientName c ++ ", we miss you!"
+        body'    = "Hi " ++ clientName c ++ ",\n\n"
+                ++ "Since you've recently turned " ++ show (clientAge c)
+                ++ ", have you checked out our latest "
+                ++ intercalate ", " (clientInterestedIn c)
+                ++ " products? Give us a visit!"
+
+server3 :: Server API
+server3 = position
+     :<|> hello
+     :<|> marketing
+
+  where position :: Int -> Int -> Handler Position
+        position x y = return (Position x y)
+
+        hello :: Maybe String -> Handler HelloMessage
+        hello mname = return . HelloMessage $ case mname of
+          Nothing -> "Hello, anonymous coward"
+          Just n  -> "Hello, " ++ n
+
+        marketing :: ClientInfo -> Handler Email
+        marketing clientinfo = return (emailForClient clientinfo)
+
+api :: Proxy API
+api = Proxy
+
+app3 :: Application
+app3 = serve api server3
+
+---------------------
+-- App3
+---------------------
+
+data HTMLLucid
+
+instance Accept HTMLLucid where
+    contentType _ = "text" // "html" /: ("charset", "utf-8")
+
+instance ToHtml a => MimeRender HTMLLucid a where
+    mimeRender _ = renderBS . toHtml
+
+-- let's also provide an instance for lucid's
+-- 'Html' wrapper.
+instance MimeRender HTMLLucid (Html a) where
+    mimeRender _ = renderBS
+
+type PersonAPI = "persons" :> Get '[JSON, HTMLLucid] [Person]
+
+data Person = Person
+  { firstName :: String
+  , lastName  :: String
+  } deriving Generic -- for the JSON instance
+
+instance ToJSON Person
+
+-- HTML serialization of a single person
+instance ToHtml Person where
+  toHtml person =
+    tr_ $ do
+      td_ (toHtml $ firstName person)
+      td_ (toHtml $ lastName person)
+
+  -- do not worry too much about this
+  toHtmlRaw = toHtml
+
+-- HTML serialization of a list of persons
+instance ToHtml [Person] where
+  toHtml persons = table_ $ do
+    tr_ $ do
+      th_ "first name"
+      th_ "last name"
+
+    -- this just calls toHtml on each person of the list
+    -- and concatenates the resulting pieces of HTML together
+    foldMap toHtml persons
+
+  toHtmlRaw = toHtml
+
+people :: [Person]
+people =
+  [ Person "Isaac"  "Newton"
+  , Person "Albert" "Einstein"
+  ]
+
+personAPI :: Proxy PersonAPI
+personAPI = Proxy
+
+server4 :: Server PersonAPI
+server4 = return people
+
+app4 :: Application
+app4 = serve personAPI server4
